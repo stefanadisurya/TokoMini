c1b95958eca3f72fbfbf670e8384ee4656272aa6
Stefan Adisurya <stefan.adisurya@tokopedia.com>
2023-03-30 10:47:24 +0700

    Initial Commit

---
+++ TokoMini/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TokoMini
+//
+//  Created by stefan.adisurya on 30/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
